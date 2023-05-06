3ebb03a39b008ae6d8c33645013845ee189be43a
Martín Morondo <martinmorondo@gmail.com>
2023-05-06 16:36:56 -0300

    add addToCart and decreaseQty functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,48 @@
+import { useState } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/header/Header';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import Pages from './pages/Pages';
+import Data from './components/Data';
+import Cart from './common/Cart/Cart';
+
 
 function App() {
+  const { productItems } = Data;
+  
+  const [cartItem, setCartItem] = useState([]);
+  
+  const addToCart = (product) => {
+    const productExist = cartItem.find((item) => item.id === product.id);
+
+    if (productExist) {
+      setCartItem(cartItem.map((item) => (item.id === product.id ? { ...productExist, qty: productExist.qty + 1 } : item)));
+    } else {
+      setCartItem([...cartItem, { ...product, qty: 1 }]);
+    }
+  }
+
+  const decreaseQty = (product) => {
+    const productExist = cartItem.find((item) => item.id === product.id);
+
+    if (productExist.qty === 1) {
+      setCartItem(cartItem.filter((item) => item.id !== product.id));
+    } else {
+      setCartItem(cartItem.map((item) => (item.id === product.id ? { ...productExist, qty: productExist.qty - 1 } : item)));
+    }
+  }
+
   return (
    <>
     <Router>
-    <Header />
+    <Header cartItem={cartItem}/>
     <Switch>
-        {/* <Route path = '/' exact>
-          <About />
-        </Route> */}
+        <Route path = '/' exact>
+          <Pages productItems={productItems} addToCart={addToCart} />
+        </Route>
+        <Route path = '/cart' exact>
+          <Cart cartItem={cartItem} addToCart={addToCart} decreaseQty={decreaseQty} />
+        </Route>
       </Switch>
       </Router>
    </>
