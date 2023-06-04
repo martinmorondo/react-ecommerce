2c631c6e172054d25fbaf050bb28e3244d80587e
Martín Morondo <martinmorondo@gmail.com>
2023-06-04 20:28:50 -0300

    add ids

--- src/components/top/Tdata.js
+++ src/components/top/Tdata.js
@@ -1,29 +1,41 @@
-const Tdata = [
+const Tdata = {
+  topItems: [
     {
+      id: 1,
       cover: "https://avatars.mds.yandex.net/i?id=64f383e9ded9c7585bfbec37db651d9d-5621412-images-thumbs&n=13",
       para: "headphone",
       desc: "3k orders this week",
+      price: 180,
     },
     {
+      id: 2,
       cover: "https://avatars.mds.yandex.net/i?id=c72e7bfdf9b6e8e2147cbe52254edca6b166b129-9106864-images-thumbs&n=13",
       para: "watch",
       desc: "4k orders this week",
+      price: 180,
     },
     {
+      id: 3,
       cover: "https://avatars.mds.yandex.net/i?id=fce9850b5f2f61cd9e90ec9439b924a6cdb1da37-9123854-images-thumbs&n=13",
       para: "sunglass",
       desc: "6k orders this week",
+      price: 180,
     },
     {
+      id: 4,
       cover: "https://avatars.mds.yandex.net/i?id=2adbe3f81e906e33270cece15d020f3645687dd2-9098096-images-thumbs&n=13",
       para: "headphone",
       desc: "4k orders this week",
+      price: 180,
     },
     {
+      id: 5,
       cover: "https://yandex-images.clstorage.net/t56Hzz131/93c61dIsQ8jF/YJp2Jmlo-0ONGCFsgAEgJXwc3HxtdHN_pbjHAQn7jDEQSRvJ3z872KY1NlNQQEjMSj3y1AOd8lG3c1PT7YOM4CHOsnLI_b6HjN1h7LBRAfCoLc25zTGi24B9gwGZXhxkA--sW51ZmU4RsnFEtJUNGW88MyFjfRGXAycXV3oziAVheUdF_NjrMgrpcl9y0RFBjuwtmD6zoQ-j_ELXWqt7MDLNzlmtairtwQETVwfkfdhPXQewIa9wpbc0xpR48xjWEcqFJM26-iE5u_J9YgEwg4_cD1gfwwEtUq_CdYjamkCxfB4I37noHZADEeV1dfy4vE_1k8NOgydlVGWkqkK7x_GrFzefmllw6ajjP2IQIMI9bC14LRDy7rJ-IFRZaEnQYT9sau24iqwwYBKE58V92g285ZHyHOEmp3ZnxLoymSfAqqdlnfj5UFp5M64wACKB3d8uSQ7BYr1jLzBmm6rJIXAtzdiM2IjdIRBgxdXVTcgvTseBMZ9xV_U0BJapwHqGIrhXhX7YqfBbujFvQxDz404d_qrOwRC-YS9CVUr5WBACDs8KLPlpnbECIMU0VS-b_O2Ws3Ou4oYEtCSXiEKbZBOq9cY82ppwWoqgfDCy47DeHey4fJCg_OPtYyU5-5mRI08f2e_ayT-xEvKFZjfvmG9_VaEBzFLWBTXW5avD-TXAqOWF3xh7wCqJIB4R4BOy3O_dux2hMm6wPnMlOympAJEuDNtdaAttEWMhdcQ0vMttPYZSYQzStIW3NpTKYvsWQ2pEZh1Ia9P7SRFdUFPgs1_MbSkN8qLu4x4RFOlYSYAhjfwpPEt7D_FRYQQnxB_rrz_2wEIv0ITHlye36DOKJVP5dgf_eLvjuTsRTXKjgwIP7Q8ovHPBb4MswrcLqopwQC082v9Y-S1Qg0KEd1S96RxO5KJTrLJWlLfVVfvhGMRjWOdFfur6oZuaod9CUTFgvh7PKk1CE7zxHmFGE",
       para: "sunglass",
       desc: "6k orders this week",
+      price: 180,
     },
-  ]
+  ],
+}
   
   export default Tdata
